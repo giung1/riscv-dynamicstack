5c5f3dbd99563416fceedf012387b69867342540
giung1 <agus_98_jov@hotmail.com.ar>
2024-06-12 15:38:36 -0300

    replaced fibonacci example for an easiest one to run

---
+++ xv6-riscv/user/testSumFirstN.c
@@ -0,0 +1,25 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+int SumFirstN(int n, int i);
+
+int main(int argc, char const *argv[])
+{   
+    int n = 400;
+    int i= 1;
+    long res = SumFirstN(n,i);
+    printf("Result of factorial of %d: %d \n", n, res);
+    return 0;
+}
+
+int SumFirstN(int n, int i)
+{
+    printf("Recursive call: %d \n", i);
+    i++;
+    if (n == 0)
+        return 0;
+    else
+        return n + SumFirstN(n-1, i);
+}
